49f248fefcf0279d2876d8f0684536b1d3bcf8d7
jpnm561 <54124048+jpnm561@users.noreply.github.com>
2019-08-26 13:19:48 -0500

    Create pDrivefunctions.py
    Python functions using pydrive's features.

---
+++ DataBaseDownload/pDrivefunctions.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 18:39:11 2019
+
+@author: José Pablo
+"""
+
+#A function that gets a document's (or folder's) drive id
+#f_name : file name, p_id : parent id, drive : drive object being used
+def fileFinder(f_name, p_id, drive):
+    f_id = ''
+    file_list = drive.ListFile({'q': "'" + p_id +"' in parents and trashed=false"}).GetList()
+    for file in file_list:
+        if file['title'] == f_name:
+            f_id = file['id']
+            break
+    return f_id
+#end of fileFinder
+
+#A function that Downloads files from Google Drive
+#f_name : file name, f_id : file id, path : directory in which to save the file, drive : the drive object being used
+def fileDownloader(f_name, f_id, path, drive):
+    file_d = drive.CreateFile({'id': t_id})
+    file_d.GetContentFile(path + f_name)    
+#end of fileDownloader
