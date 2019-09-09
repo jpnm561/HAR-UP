ef9da52973ff63e926fb0ac9cd0811bd7156ef24
jpnm561 <54124048+jpnm561@users.noreply.github.com>
2019-09-08 21:27:48 -0500

    Update BC_Training.py

--- BC_Training.py
+++ BC_Training.py
@@ -14,9 +14,8 @@ from createFolder import createFolder
 import numpy as np
 import random as rnd
 import os
-import itertools
 import matplotlib.pyplot as plt
-
+from scorePlots import plotScore, plot_confusion_matrix
 
 def BC_Training(concept,
             t_window=['1&0.5','2&1','3&1.5'],
@@ -209,27 +208,6 @@ def BC_Training(concept,
                         print('-----' + str(a) + '/' + str(len(d_base)-1))
                         print('----...Prediction ' + str(k+1) + ' ' + method + ' finished')
 
-"""
-Confusion matrix code taken from scikit-learn.org
-https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
-"""
-def plot_confusion_matrix(cm,classes,normalize=True,title='Confusion matrix',cmap=plt.cm.Blues):
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:,np.newaxis]
-    plt.imshow(cm,interpolation='nearest',cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks,classes,rotation=45)
-    plt.yticks(tick_marks,classes)
-    fmt = '.2f'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]),range(cm.shape[1])):plt.text(j,i,format(cm[i,j],fmt),
-                                        horizontalalignment='center',
-                                        color='white' if cm[i,j] > thresh else 'black')
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-    plt.tight_layout()
 
 def write_score(w, score, arr,ln_flg = False):
     w.write(score + '\n')
@@ -381,6 +359,8 @@ def BC_Scores(concept,
         except Exception as e:
             print(e)
         w.close()
+        #The scores for evey time window and method are plotted in a bar graph
+        plotScore([acc_e,ppv_e,rec_e,fsc_e],cncpt,t_window=t_window,methods=methods)
         #The average confusion matrix is plotted (avg time window for every method)
         for i in range(0,len(methods)):
             #true positive
